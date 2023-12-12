d7f968500b55459974174040375e37a1eb391baf
LorelessVictory <ejboutens1337@gmail.com>
2023-12-12 12:36:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+
+import './App.css'
+
+function App() {
+
+
+    return (
+        <>
+            <section className="outer-container">
+                <nav className="nav">
+                </nav>
+                <main>
+                    <section className="random-game">
+
+                    </section>
+                    <section className="content">
+
+                    </section>
+                </main>
+                <footer>
+                </footer>
+            </section>
+        </>
+    )
+}
+
+export default App
