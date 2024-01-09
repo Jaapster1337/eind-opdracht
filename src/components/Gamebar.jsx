68bfc10af48d8bcdba239575b33338d2bbe0fa6b
LorelessVictory <ejboutens1337@gmail.com>
2024-01-09 11:33:09 +0100

    tussentijdse commit. bezig met achtergrond van GameBar

--- src/components/Gamebar.jsx
+++ src/components/Gamebar.jsx
@@ -1,28 +1,39 @@
 import {getRandomGame} from "../helpers/getRandomGame.js";
 import {useEffect, useState} from "react";
+
 export function Gamebar() {
+
     const [game, setGame] = useState(null)
+
     useEffect(() => {
-        getRandomGame()
-            .then((randomGame) => {setGame(randomGame)
-            })
-            .catch((e) => {
+        async function fetchRandomGame() {
+            try {
+                const randomGame = await getRandomGame();
+                setGame(randomGame)
+            } catch (e) {
                 console.error("Error fetching random game:", e)
-            })
-        }, []);
+            }
+        }
+        fetchRandomGame();
+    }, []);
     console.log(game)
 
     return (
         <>
             <section className="Gamebar-container">
-                <div className="game-img">
-                    <img src="" alt="game cover art"/>
+                <div className="game-img" style={{backgroundImage: `url('${game ? game.background_image : ''}')`}}>
+                    {/*{game && <img src="" alt="game cover art"/>}*/}
                 </div>
+                {/*<script>*/}
+                {/*    const imagePath = {game.background_image};*/}
+                {/*    const element = document.getElementById('background-target');*/}
+                {/*    element.style.backgroundImage = `url('$imagePath')`;*/}
+                {/*</script>*/}
                 <div className="game-info">
-                    <p>game name</p>
-                    <p>publisher</p>
-                    <p>release date</p>
-                    <p>game genre</p>
+                    <p>{game ? game.name : 'Game Name'}</p>
+                    <p>{game ? game.publishers['name'] : 'Publisher'}</p>
+                    <p>{game ? game.released : 'Release Date'}</p>
+                    <p>{game ? game.genre : 'Game Genre'}</p>
                 </div>
             </section>
         </>
--- src/helpers/getRandomGame.js
+++ src/helpers/getRandomGame.js
@@ -2,31 +2,33 @@ import axios from "axios";
 
 export async function getRandomGame() {
     let numberOfGames = 0
-    let randomGamePick = {}
     try {
         //get total number of games
         const totalGames = await axios.get(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_REACT_API_KEY}`)
         numberOfGames = totalGames.data.count
-        await randomGame()
+        return await randomGame(numberOfGames)
     } catch (e) {
         console.log("Error occured", e)
+        throw e;
     }
+}
 
-    //request game with gameid randomNum and while request status !== 200 retry until 200
-    async function randomGame() {
+//request game with gameid randomNum and while request status !== 200 retry until 200
+async function randomGame(numberOfGames) {
+    let randomGamePick = {}
+    let validPage = false;
 
-        let validPage = false;
-        while (!validPage) {
-            const randomNum = Math.floor(Math.random() * numberOfGames)
-            try {
-                randomGamePick = await axios.get(`https://api.rawg.io/api/games/${randomNum}?key=${import.meta.env.VITE_REACT_API_KEY}`)
-                if(randomGamePick.status === 200){
-                    validPage = true;
-                }
-            } catch (e) {
-                console.error(e)
+    while (!validPage) {
+        const randomNum = Math.floor(Math.random() * numberOfGames)
+        try {
+            const response = await axios.get(`https://api.rawg.io/api/games/${randomNum}?key=${import.meta.env.VITE_REACT_API_KEY}`)
+            if (response.status === 200) {
+                randomGamePick = response.data
+                validPage = true;
             }
+        } catch (e) {
+            console.error(e)
         }
     }
-    return randomGamePick;
+return randomGamePick;
 }
